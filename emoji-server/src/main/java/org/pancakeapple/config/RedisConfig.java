c3ef211599266096502ca76637cb25841ce21fee
ThatBlueSky2018 <3010796910@qq.com>
2023-10-12 22:36:03 +0800

    引入Redis并配置，引入开发环境和生产环境配置文件

---
+++ emoji-server/src/main/java/org/pancakeapple/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package org.pancakeapple.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("配置Redis模板对象...");
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
--- emoji-server/src/test/java/org/pancakeapple/EmojiServerApplicationTests.java
+++ emoji-server/src/test/java/org/pancakeapple/EmojiServerApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.pancakeapple.properties.AliOSSProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.sql.DataSource;
 
@@ -15,9 +16,17 @@ class EmojiServerApplicationTests {
     @Autowired
     private AliOSSProperties aliOSSProperties;
 
+    @Autowired
+    private RedisTemplate<Object,Object> redisTemplate;
+
     @Test
     void contextLoads() {
         System.out.println(dataSource);
         System.out.println(aliOSSProperties.getBucketName());
+    }
+
+    @Test
+    void testRedis() {
+        redisTemplate.opsForValue().set("test","sky");
     }
 }
